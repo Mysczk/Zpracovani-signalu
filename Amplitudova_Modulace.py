66df58cefa90b76a42f5eee75980619346e35948
Mysczk <michal.soukup.jr@gmail.com>
2025-02-04 17:24:00 +0100

    Big changes

--- Amplitudova_Modulace.py
+++ Amplitudova_Modulace.py
@@ -1,35 +1,55 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def Draw_ampl(data1, data2, data3, points):
+    plt.plot(points, data1, label = "Amplitude")
+    plt.plot(points, data2, label = "Modulation")
+    plt.plot(points, data3, label = "Finalization")
+    plt.title("Signal Modulation")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.legend(loc='upper right')
+    plt.show()
 
-def Am(A, f, t):
-    return A * np.sin((2 * np.pi * f) * t)
+def Nosna(A, f, t):
+    return np.array(A * np.sin((2 * np.pi * f) * t))
 
 def M(M, fi, t, eps = 0):
-    return M * np.sin((fi * t) + eps)
+    return (M * np.sin((fi * t) + eps))
 
 def Am_Mod(M, A, f, t, eps = 0):
-    return (M + A) * np.sin((2 * np.pi * f) * t)
+    return np.array((M + A) * np.sin((2 * np.pi * f) * t))
 
 
-A = 1
+def frek_nos(f, t):
+    w = (2 * np.pi * f)
+    return np.array(np.sin(w * t))
+
+
+def frek_mod(A, f, fm, t):
+    w = (2 * np.pi * f)
+    wd = (2 * np.pi * fm)
+    return np.array(A*np.sin(w * t  + A * np.cos(wd * t)))
+
+A = 2
 M_val = 4
 f = 10
-t = 10
+fm = 1
+t = 2
 eps = 0
 partitions = 10000
 
 
 points = np.array(np.linspace(0, t, partitions))
-data = Am(A,f,points)
+data1 = Nosna(A,f,points)
 data2 = Am_Mod(M(M_val, f, points), A, f, points)
-data3 = data+data2
+data3 = data1+data2
 
-plt.plot(points, data, label = "Aplitude")
-plt.plot(points, data2, label = "Modulation")
-plt.plot(points, data3, label = "Finalization")
-plt.title("Signal Modulation")
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
-plt.legend(loc='upper right')
+frek_nos = frek_nos(f,points)
+frek_mod = frek_mod(A, f, fm, points)
+plt.plot(points, frek_nos, label = "nosna")
+plt.plot(points, frek_mod, label = "final")
+plt.legend()
 plt.show()
+
+#Draw_ampl(data1, data2, data3, points)
